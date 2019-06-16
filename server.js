ac2e03b34f711d84a6ba1cea9fd6cd34cbc0e2d9
Vladimir Turkonja <turkonjavlada@gmail.com>
2019-06-16 14:05:12 +0200

    Add all necessary routes

--- server.js
+++ server.js
@@ -10,9 +10,16 @@ connectDB();
 /* Middleware */
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('hello');
-});
+/* Routes */
+const users = require('./routes/api/users');
+const posts = require('./routes/api/posts');
+const profile = require('./routes/api/profile');
+const auth = require('./routes/api/auth');
+
+app.use('/api/users', users);
+app.use('/api/auth', auth);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
 
 app.listen(PORT, process.env.IP, () => {
   console.log('Server is running');
