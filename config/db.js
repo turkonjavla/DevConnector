5700c322047116893164a2206673d34fa8bbbb69
Vladimir Turkonja <turkonjavlada@gmail.com>
2019-06-17 17:03:04 +0200

    Update db file and set useFindAndModify to true

--- config/db.js
+++ config/db.js
@@ -9,7 +9,7 @@ const connectDB = async () => {
       {
         useNewUrlParser: true,
         useCreateIndex: true,
-        useFindAndModify: true
+        useFindAndModify: false
       }
     );
     console.log('MongoDB connected')
